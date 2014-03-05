59bc4bda80eff63c01c6261ed5332261246a3912
Rex St John <rexstjohn@ux-rx.com>
2014-03-05 11:36:49 -0800

    Fixing bugs

--- camera/src/main/java/com/ultimate/camera/NavigationDrawerFragment.java
+++ camera/src/main/java/com/ultimate/camera/NavigationDrawerFragment.java
@@ -269,11 +269,6 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
--- camera/src/main/java/com/ultimate/camera/activities/MainActivity.java
+++ camera/src/main/java/com/ultimate/camera/activities/MainActivity.java
@@ -169,9 +169,6 @@ public class MainActivity extends CameraActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
